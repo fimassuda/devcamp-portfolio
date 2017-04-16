eb6a4894ba5ff644fcea8b6bcb80b66800ff7212
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-15 21:59:59 -0300

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    FidevViewTool::Renderer.copyright 'Fillipe Massuda', 'All rights reserved'
+  end
+
 end

0825ca62b8b43965e4be672e5057050d08178c03
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-18 20:34:20 -0300

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,20 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Fillipe Massuda Portfolio", sticky: false)
+  end
+
+
+
+
+
 end

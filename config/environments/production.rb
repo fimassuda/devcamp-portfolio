d10e70b37abbe9e8afcb3559abbdb3d03aab1d26
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-20 08:20:25 -0300

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,11 +34,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -83,4 +78,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  config.action_cable.url = 'wss://fillipe-portfolio.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://fillipe-portfolio.herokuapp.com', 'http://fillipe-portfolio.herokuapp.com' ]
+
 end
